9974e63bd72037b7d3f6834806ec620eb299bd48
Fletch153 <michael.fletcher153@gmail.coim>
2019-02-18 16:02:23 +0000

    Classifier Tutorial

--- DeepLearningIntro/Tutorial.py
+++ DeepLearningIntro/Tutorial.py
@@ -19,7 +19,7 @@ model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=3)
+model.fit(x_train, y_train, epochs=100)
 
 
 predictions = model.predict([x_test])
